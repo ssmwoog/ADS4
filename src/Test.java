0fcaa657fb29eda13ff51bc0eda754c65f086563
shuga <124703939+ssmwoog@users.noreply.github.com>
2023-05-12 23:10:35 +0600

    added some changes to test class

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,8 @@ public class Test {
     public static void main(String[] args) {
         BST<Integer, String> tree = new BST<>();
         tree.put(2, "two");
-        tree.put(1, "one");
-        tree.put(3, "three");
+        tree.put(7, "nine");
+        tree.put(9, "six");
         for (BST<Integer, String>.Node node : tree) {
             System.out.println("key is " + node.key + " and value is " + node.val);
         }
